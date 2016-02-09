ebf7d627336487357d1efde5eaa68f7a33d1d18d
Charlie Egan <git@charlieegan3.com>
2016-02-09 16:29:03 +0000

    Rename and remove 4040 references in files
    Motivation:
    -No longer making use of the cls files

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-rm cs4040report.pdf
+rm report.pdf
 rm *.bbl
 rm *.aux
-pdflatex cs4040report
-bibtex cs4040report
-pdflatex cs4040report
-pdflatex cs4040report
-pdflatex cs4040report
-rm cs4040report.aux  cs4040report.blg  cs4040report.dvi  cs4040report.log
-open cs4040report.pdf
-detex cs4040report.tex | wc -w
-texcount cs4040report.tex
+pdflatex report
+bibtex report
+pdflatex report
+pdflatex report
+pdflatex report
+rm report.aux  report.blg  report.dvi  report.log
+open report.pdf
+detex report.tex | wc -w
+texcount report.tex
